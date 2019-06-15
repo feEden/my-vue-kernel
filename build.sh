ecaed3e5cee35693d904a9b83071484165ea6b5e
songsong <17317112520@163.com>
2019-06-15 16:49:42 +0800

    修改buish.sh,检查命令不存在时自动安装

--- build.sh
+++ build.sh
@@ -1,4 +1,36 @@
 #! /bin/bash
+
+hasCommandByType(){
+    if type $1 2>/dev/null
+    then
+        return 1
+    else
+        return -1
+    fi
+}
+hasCommandByType cross-env
+returnVue=$?
+
+if [ $returnVue == -1 ]
+then
+    `sudo npm install cross-env -g`
+fi
+
+hasCommandByType gulp
+let returnVue=$?
+
+if [ $returnVue == -1 ]
+then
+    `sudo npm install gulp -g`
+fi
+
+hasCommandByType webpack-cli
+let returnVue=$?
+if [ $returnVue == -1 ]
+then
+    `sudo npm install webpack-cli -g`
+fi
+
 npm install
 npm run deploy:test
 npm run client:build
---
+++ scripts/deploy/run.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+pm2 start dist/app.js
